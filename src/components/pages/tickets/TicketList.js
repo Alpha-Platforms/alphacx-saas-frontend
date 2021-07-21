08fcb74a78336b7ccd023a09de4a3f3e0531d508
Gideon Idoko <iamgideonidoko@gmail.com>
2021-07-21 18:28:57 +0100

    update ticket table

--- src/components/pages/tickets/TicketList.js
+++ src/components/pages/tickets/TicketList.js
@@ -40,6 +40,11 @@ const TicketPagination = props => {
     );
   }
 
+  const handleExportBtn = () => {
+      const exportBtn = document.querySelector('.MuiButtonBase-root.MuiIconButton-root.MuiIconButton-colorInherit');
+      exportBtn.click();
+  }
+
 const TicketList = ({isTicketsLoaded, tickets}) => {
     const [ticketLoading,
         setTicketLoading] = useState(false);
@@ -58,21 +63,7 @@ const TicketList = ({isTicketsLoaded, tickets}) => {
                     className="d-flex justify-content-between flex-wrap bg-light rounded-top-big flex-md-nowrap align-items-center p-4">
 
                     <div>
-                        <div className="input-group input-group-sm has-validation">
-
-                            <span className="input-group-text bg-transparent border-end-0">
-
-                                <i className="bi-search"></i>
-
-                            </span>
-
-                            <input
-                                type="text"
-                                className="form-control bg-transparent border-start-0 pe-4"
-                                placeholder="Search all contacts"
-                                required=""/>
-
-                        </div>
+                        
                     </div>
 
                     <div className="btn-toolbar mb-md-0">
@@ -81,135 +72,19 @@ const TicketList = ({isTicketsLoaded, tickets}) => {
                             class="btn btn-sm bg-at-blue px-md-3 mx-1"
                             data-bs-toggle="modal"
                             data-bs-target="#createNewTicket">
-                            <img src={TicketStarIcon} alt=""/>
-                            New Ticket
+                            <img src={TicketStarIcon} alt=""/>&nbsp;New Ticket
                         </button>
 
                         <button
+                            onClick={handleExportBtn}
                             type="button"
                             className="btn btn-sm btn-outline-secondary px-md-3 mx-md-3">
                             <ImportSvg/>&nbsp;Export
                         </button>
-                        <div className="btn-group me-2">
-                            <button
-                                type="button"
-                                disabled={false}
-                                className="btn btn-sm btn-outline-secondary">
-                                <i className="bi-chevron-left"></i>
-                            </button>
-                            <button
-                                type="button"
-                                disabled={false}
-                                className="btn btn-sm btn-outline-secondary">
-                                <i className="bi-chevron-right"></i>
-                            </button>
-                        </div>
                     </div>
 
                 </div>
 
-                {/* <table
-                    id="usersTable"
-                    className="table table-responsive bg-white rounded-bottom-big table-striped__ table-hover__ w-100 p-0 pt-0">
-                    <thead className="bg-light">
-                        <tr>
-                            <th className="text-center">
-                                <input type="checkbox" className="form-check-input ticket-select-all" id=""/>
-                            </th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Category</th>
-                            <th>Ticket ID</th>
-                            <th>State</th>
-                            <th>Priority</th>
-                            <th>Status</th>
-                            <th>Created</th>
-                            <th>
-
-                                <EditSvg/>
-
-                                <img src={FilterIcon} alt=""/>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>
-                                <input type="checkbox" className="form-check-input ticket-select" id=""/>
-                            </td>
-                            <td className="text-at-blue-light">Jerome Bell</td>
-                            <td>debra.holt@example.com</td>
-                            <td>whitefish664</td>
-                            <td className="text-at-blue-light">3398</td>
-
-                            <td className="ticket-state yellow">
-                                <Link href="#" className="btn btn-sm ">Pending</Link>
-                            </td>
-                            <td className="ticket-priority green">
-
-                                <div className="dropdown">
-                                    <button
-                                        className="btn btn-sm dropdown-toggle pt-0"
-                                        type="button"
-                                        id="dropdownMenuButtonSM"
-                                        data-bs-toggle="dropdown"
-                                        aria-expanded="false">
-                                        <span>Low</span>
-                                    </button>
-                                    <ul className="dropdown-menu bg-at-blue" aria-labelledby="dropdownMenuButtonSM">
-                                        <li>
-                                            <Link className="dropdown-item" href="#">Low</Link>
-                                        </li>
-                                        <li>
-                                            <Link className="dropdown-item" href="#">Medium</Link>
-                                        </li>
-                                        <li>
-                                            <Link className="dropdown-item" href="#">High</Link>
-                                        </li>
-                                        <li>
-                                            <Link className="dropdown-item" href="#">Urgent</Link>
-                                        </li>
-                                    </ul>
-                                </div>
-
-                            </td>
-
-                            <td className="ticket-status green">
-
-                                <div className="dropdown">
-                                    <button
-                                        className="btn btn-sm dropdown-toggle pt-0"
-                                        type="button"
-                                        id="dropdownMenuButtonSM"
-                                        data-bs-toggle="dropdown"
-                                        aria-expanded="false">
-                                        <span>Resolved</span>
-                                    </button>
-                                    <ul className="dropdown-menu bg-at-blue" aria-labelledby="dropdownMenuButtonSM">
-                                        <li>
-                                            <Link className="dropdown-item" href="#">Open</Link>
-                                        </li>
-                                        <li>
-                                            <Link className="dropdown-item" href="#">Pending</Link>
-                                        </li>
-                                        <li>
-                                            <Link className="dropdown-item" href="#">Resolved</Link>
-                                        </li>
-                                        <li>
-                                            <Link className="dropdown-item" href="#">Closed</Link>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </td>
-                            <td>5/27/15</td>
-                            <td>
-                                <img src={ShowIcon} alt=""/>
-                            </td>
-
-                        </tr>
-
-                    </tbody>
-                </table> */}
 
                 <div id="ticketsTable" className="pb-5">
                     {isTicketsLoaded && <MaterialTable
@@ -236,7 +111,7 @@ const TicketList = ({isTicketsLoaded, tickets}) => {
                             }, {
                                 title: 'State',
                                 field: 'state',
-                                render: rowData => <div className="ticket-state yellow"><Link to="#" className="btn btn-sm" style={{ backgroundColor: rowData.state.background_color, color: rowData.state.foreground_color }}>{rowData.state.status}</Link></div>
+                                render: rowData => <div className="ticket-state yellow"><Link to="#" className="btn btn-sm" style={{ color: rowData.state.foreground_color }}>{rowData.state.status}</Link></div>
                             }, {
                                 title: 'Status',
                                 field: 'status',
@@ -252,9 +127,6 @@ const TicketList = ({isTicketsLoaded, tickets}) => {
                             }, {
                                 title: 'Created',
                                 field: 'created'
-                            }, {
-                                title: '',
-                                field: 'action'
                             }
                         ]
                     }
@@ -271,7 +143,9 @@ const TicketList = ({isTicketsLoaded, tickets}) => {
                     }
                     options = {{
                         search: true,
-                        selection: true
+                        selection: true,
+                        exportButton: true,
+                        filtering: true
                     }}
                     components={{ 
                         Pagination: TicketPagination
