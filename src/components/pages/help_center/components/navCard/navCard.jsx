b04ea6cfb4283aaa37be02a13612f7fa73e9ff1a
venaz-dev <chibuzo.madu90@gmail.com>
2021-07-22 20:55:02 +0100

    help center home page

---
+++ src/components/pages/help_center/components/navCard/navCard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { HelpNavIcon } from "../../../../../assets/images/svgs";
+import "./navCard.scss";
+
+const NavCard = ({ title, icon, items }) => {
+  return (
+    <div className="nav-card">
+      <div>
+        <HelpNavIcon name={icon} />
+      </div>
+      <p className="title">{title}</p>
+      <div className="description">
+        {items.map((item, i) => (
+          <p key={i}>{item}</p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default NavCard;
