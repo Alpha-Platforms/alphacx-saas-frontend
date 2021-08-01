9ae740f8faa3ea7101be72ac54b26efe1d4f6e75
codeuiandy <frostandy41@gmail.com>
2021-08-01 23:41:29 +0100

    chat ui V2

--- src/assets/images/svgs.js
+++ src/assets/images/svgs.js
@@ -2932,3 +2932,31 @@ export const UserProfileIcon3 = ({ size }) => {
     </svg>
   );
 };
+
+export const ExpandChat = ({ size }) => {
+  return (
+    <svg
+      width="30"
+      height="30"
+      viewBox="0 0 30 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle opacity="0.1" cx="15" cy="15" r="15" fill="#0067DD" />
+      <path
+        d="M11.5 10.75L15 7.25L18.5 10.75"
+        stroke="#0067DD"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M18.5 19.25L15 22.75L11.5 19.25"
+        stroke="#0067DD"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
