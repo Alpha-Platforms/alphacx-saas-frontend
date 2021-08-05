9161aec57f4d2d87f2eb6fb08fe96b33cc9aa7a6
Gideon Idoko <iamgideonidoko@gmail.com>
2021-08-05 12:29:10 +0100

    add user personal settings

--- src/components/pages/tickets/Ticket.js
+++ src/components/pages/tickets/Ticket.js
@@ -1,4 +1,4 @@
-import {useState, Fragment, useEffect} from 'react';
+import {useState, Fragment, useEffect, useContext} from 'react';
 import TicketIcon from '../../../assets/svgicons/Ticket.svg';
 import WorkIcon from '../../../assets/svgicons/Work.svg';
 import ProfileLightIcon from '../../../assets/svgicons/Profile-Light.svg';
@@ -10,12 +10,45 @@ import {useParams} from 'react-router-dom';
 import {getCurrentTicket} from '../../../reduxstore/actions/ticketActions';
 import {getUserInitials} from '../../../helper';
 import UserProfile from '../conersations/userProfile';
+import { dateFormater } from "../../helpers/dateFormater";
+import {
+  StarIconTicket,
+  SendMsgIcon,
+  ExpandChat,
+} from "../../../assets/images/svgs";
+import moment from 'moment';
+import { capitalize } from '@material-ui/core';
+import ClipLoader from "react-spinners/ClipLoader";
+import { EditorState, convertToRaw, ContentState } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import draftToHtml from "draftjs-to-html";
+import BackArrow from "../../../assets/imgF/back.png";
+import editorImg from "../../../assets/imgF/editorImg.png";
+import Smiley from "../../../assets/imgF/Smiley.png";
+import boldB from "../../../assets/imgF/boldB.png";
+import TextItalic from "../../../assets/imgF/TextItalic.png";
+import TextUnderline from "../../../assets/imgF/TextUnderline.png";
+import capitalizeFirstLetter from "../../helpers/capitalizeFirstLetter";
+import { SocketDataContext } from "../../../context/socket";
+import { NotificationManager } from 'react-notifications';
+import {
+  httpGetMain,
+  httpPostMain,
+  httpPatchMain,
+} from "../../../helpers/httpMethods";
+
 
 const CircleIcon = (props) => <span className="cust-grey-circle"><img src={props.icon} alt="" className="pe-none"/></span>;
 
 const Ticket = ({isTicketLoaded, getCurrentTicket, isCurrentTicketLoaded, currentTicket}) => {
 
     const {id} = useParams();
+    const [SenderInfo, setSenderInfo] = useState({});
+
+    const initialState = EditorState.createWithContent(
+      ContentState.createFromText("")
+    );
 
     useEffect(() => {
         // get current ticket when component mounts
@@ -24,7 +57,220 @@ const Ticket = ({isTicketLoaded, getCurrentTicket, isCurrentTicketLoaded, curren
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
+    useEffect(() => {
+      if (currentTicket) {
+        const {customer, subject} = currentTicket;
+        setSenderInfo(currentTicket);
+      }
+
+    }, [currentTicket]);
+
     // console.log("current ticket from tick", currentTicket);
+
+
+
+    /** >>>>> FROM CODE-UI-ANDY */
+
+    const [loadSelectedMsg, setloadSelectedMsg] = useState("");
+    const [tickets, setTickets] = useState([]);
+    const [filterTicketsState, setFilterTicketsState] = useState([]);
+    const [ticket, setTicket] = useState([]);
+    const [LoadingTick, setLoadingTicks] = useState(true);
+    const [loadSingleTicket, setLoadSingleTicket] = useState(false);
+    // const [SenderInfo, setSenderInfo] = useState(false);
+    const [singleTicketFullInfo, setTingleTicketFullInfo] = useState(false);
+    const [Category, setCategory] = useState([]);
+    const [editorState, setEditorState] = useState(initialState);
+    const [firstTimeLoad, setfirstTimeLoad] = useState(true);
+    const [MessageSenderId, setMessageSenderId] = useState("");
+    const [TicketId, setTicketId] = useState("");
+    const [showUserProfile, setshowUserProfile] = useState(false);
+    const [ReplyTicket, setReplyTicket] = useState({
+      plainText: "",
+      richText: "",
+    });
+    const [Statues, setStatues] = useState([]);
+    const [UserInfo, setUserInfo] = useState({});
+    const [ChatCol, setChatCol] = useState({
+      col1: "",
+      col2: "",
+    });
+    const [openSaveTicketModal, setopenSaveTicketModal] = useState(false);
+    const [filterChat, setFilterChat] = useState("system");
+    const [saveTicket, setSaveTicket] = useState({
+      customer: "",
+      subject: "",
+      description: [],
+      category: "",
+    });
+
+    const [sendingReply, setsendingReply] = useState(false);
+    const [msgHistory, setMsgHistory] = useState([]);
+    const [wsTickets, setwsTickets] = useState([]);
+    const [categoryUpdate, setCategoryUpdate] = useState("");
+    const [noResponseFound, setNoResponseFound] = useState(true);
+    const [TodayMsges, setTodayMsges] = useState([]);
+    const [YesterdayMsges, setYesterdayMsges] = useState([]);
+    const [AchiveMsges, setAchiveMsges] = useState([]);
+    const [ShowAchive, setShowAchive] = useState(false);
+    const [channel, setchannel] = useState("All");
+    const [status, setstatus] = useState("All");
+
+
+    const {
+      AppSocket,
+      //wsTickets,
+      setWsTicketFilter,
+      wsTicketFilter,
+      // setMsgHistory,
+      // msgHistory,
+    } = useContext(SocketDataContext);
+
+
+    useEffect(() => {
+      AppSocket.createConnection();
+      AppSocket.io.on(`ws_tickets`, (data) => {
+        console.log("this are Ticketsss", data?.data?.tickets);
+        setwsTickets(data?.data?.tickets);
+      });
+      AppSocket.io.on(`message`, (data) => {
+        console.log("this are history msbsg", data);
+        // console.log(UserInfo);
+        let msg = {
+          created_at: data.created_at,
+          id: data.history.id,
+          plain_response: data.history.plain_response,
+          response: data.history.response,
+          type: "reply",
+          user: data.user,
+        };
+        console.log("msg>>>", msg);
+  
+        setMsgHistory((item) => [...item, msg]);
+        // sortMsges((item) => [...item, msg]);
+      });
+      return () => {
+        AppSocket.io.disconnect();
+      };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const sortMsges = (msgs) => {
+      console.log("msgHis", msgs);
+      let Today = [];
+  
+      let resultToday = msgs.filter((observation) => {
+        return (
+          moment(observation.created_at).format("DD/MM/YYYY") ==
+          moment(new Date()).format("DD/MM/YYYY")
+        );
+      });
+      let resultYesterday = msgs.filter((observation) => {
+        return (
+          moment(observation.created_at).format("DD/MM/YYYY") ==
+          moment().add(-1, "days").format("DD/MM/YYYY")
+        );
+      });
+  
+      let resultAchive = msgs.filter((observation) => {
+        return (
+          moment(observation.created_at).format("DD/MM/YYYY") !=
+            moment().add(-1, "days").format("DD/MM/YYYY") &&
+          moment(observation.created_at).format("DD/MM/YYYY") !=
+            moment(new Date()).format("DD/MM/YYYY")
+        );
+      });
+      setTodayMsges(resultToday);
+      setYesterdayMsges(resultYesterday);
+      setAchiveMsges(resultAchive);
+      console.log("Today>>>", resultToday);
+      console.log("Yesterdat msg ", resultYesterday);
+      console.log("resultAchive msg ", resultAchive);
+    };
+
+    function createMarkup(data) {
+      return { __html: data };
+    }
+
+    const onEditorStateChange = (editorState) => {
+      // handleDescriptionValidation(editorState);
+  
+      const plainText = editorState.getCurrentContent().getPlainText();
+      const richText = draftToHtml(convertToRaw(editorState.getCurrentContent()));
+      setEditorState(editorState);
+      setReplyTicket({ plainText, richText });
+      console.log(">>>>", richText, richText);
+    };
+
+    const _uploadImageCallBack = (file) => {
+      // long story short, every time we upload an image, we
+      // need to save it to the state so we can get it's data
+      // later when we decide what to do with it.
+  
+      // Make sure you have a uploadImages: [] as your default state
+      let uploadedImages = [];
+  
+      const imageObject = {
+        file: file,
+        localSrc: URL.createObjectURL(file),
+      };
+  
+      uploadedImages.push(imageObject);
+      console.log(imageObject);
+  
+      //this.setState(uploadedImages: uploadedImages)
+  
+      // We need to return a promise with the image src
+      // the img src we will use here will be what's needed
+      // to preview it in the browser. This will be different than what
+      // we will see in the index.md file we generate.
+      return new Promise((resolve, reject) => {
+        resolve({ data: { link: imageObject.localSrc } });
+      });
+    };
+
+    const replyTicket = async (reply, attachment) => {
+      console.log(reply);
+      const data = {
+        // type: "note",
+        response: reply.richText,
+        plainResponse: reply.plainText,
+        phoneNumber: singleTicketFullInfo.customer.phone_number,
+        // attachment: "",
+      };
+      console.log(singleTicketFullInfo.customer.phone_number);
+      console.log(data);
+      // setsendingReply(true);
+      const replyData = {
+        attachment: null,
+        created_at: new Date(),
+        plain_response: reply.plainText,
+        response: reply.richText,
+        // user: SenderInfo?.customer,
+        user: ticket[0]?.assignee,
+      };
+      console.log(replyData);
+      setMsgHistory((item) => [...item, replyData]);
+      const res = await httpPostMain(
+        `tickets/${singleTicketFullInfo.id}/replies`,
+        data
+      );
+      if (res?.status == "success") {
+        // setsendingReply(false);
+        // ReloadloadSingleMessage();
+        setEditorState(initialState);
+        setReplyTicket({ plainText: "", richText: "" });
+      } else {
+        // setLoadingTicks(false);
+        setsendingReply(false);
+        return NotificationManager.error(res?.er?.message, "Error", 4000);
+      }
+    };
+
+    console.log("from ticket single");
+
+
+    /* <<<<<< FROM CODE-UI-ANDY */
 
     return (
         <Fragment>
@@ -98,185 +344,16 @@ const Ticket = ({isTicketLoaded, getCurrentTicket, isCurrentTicketLoaded, curren
                     }}
                         className="bg-secondary py-3 pt-0 bg-white">
 
-<div className="col-md ps-0 pe-0">
-
-<div className="">
-
-  <div className="p-md-4">
-
-    <div className="d-flex justify-content-start rounded-2 comment">
-      <a href="#" className="conversation-avatar align-self-start acx-text-green">
-        <img src="../assets/alphatickets/images/man.jpg" className="rounded-circle img-fluid" loading="lazy" alt=""/>
-      </a>
-
-      <div className="flex-grow-1 ms-2">
-        <div className="d-flex justify-content-between">
-          <div>                         
-            <p className="text-at-blue mb-0">Marvin McKinney</p>
-            <small className="fst-italic acx-fs-8">Via email (Sat, 13 Mar 2021 at 10:54 AM)</small>
-          </div>
-          <div className="d-flex justify-content-end flex-wrap">
-
-            <div className="ms-2">
-              <button className="btn btn-sm px-1">
-                <object data="../assets/alphatickets/icons/swap.svg" className="icon-md" type="image/svg+xml"></object>
-              </button>
-              <button className="btn btn-sm px-1">
-                <object data="../assets/alphatickets/icons/user_icon.svg" className="icon-md" type="image/svg+xml"></object>
-              </button>
-              <button id="openProfilePanel" className="btn btn-sm px-1">
-                <object data="../assets/alphatickets/icons/user_rounded.svg" className="icon-md" type="image/svg+xml"></object>
-              </button>
-              <button data-bs-toggle="modal" data-bs-target="#closedChatModal" className="btn btn-sm px-1">
-                <object data="../assets/alphatickets/icons/CheckCircle.svg" className="icon-md" type="image/svg+xml"></object>
-              </button>
-            </div>
-
-          </div>
-        </div>
-        
-      </div>
-
-    </div>
-
-    <div className="mt-2">
-      <h6 className="mb-0 fw-bold">How can I get a refund for my order?</h6>
-      <p className="small">Ticket ID: #53467</p>
-    </div>
-
-    <div className="border-top pt-2">
-      <p>Hi there,</p>
-      <p>I need a refund for the headphones that I purchased last week. My order ID is #53467. The product was damaged when I received it. Can you please tell me how I can get a refund?</p>
-      <p>Best, <br/>Jerome.</p>
-
-      <small className="text-muted acx-fs-8">13 Mar 2021</small>                    
-
-    </div>
-    
-  </div>
+                        Conversation Part
 
 
-  <div className="bg-at-blue-lighter border m-md-3 p-md-2 rounded">
 
-    <div className="d-flex justify-content-start rounded-2 comment">
-      <a href="#" className="conversation-avatar align-self-start acx-text-green">
-        <img src="../assets/alphatickets/images/man.jpg" className="rounded-circle img-fluid" loading="lazy" alt=""/>
-      </a>
 
-      <div className="flex-grow-1 ms-2">
-        <div className="d-flex justify-content-between">
-          <div>                         
-            <p className="text-at-blue mb-0">Hammed Daudu <span className="text-muted">sent an internal note to Steph Okafor & 23 Others</span></p>
-            <small className="fst-italic acx-fs-8">16 days ago (Sat, 13 Mar 2021 at 12:45 PM)</small>
-          </div>
 
-          
-        </div>
-        
-      </div>
-
-    </div>
-
-    <div>
-      <p><span className="text-at-blue">@Steph okafor</span> Please could you take a look at this.</p>
-    </div>
-    
-  </div>
-
-  <div className="p-md-4">
-
-    <div className="d-flex justify-content-start rounded-2 comment">
-      <a href="#" className="conversation-avatar align-self-start acx-text-green">
-        <img src="../assets/alphatickets/images/man.jpg" className="rounded-circle img-fluid" loading="lazy" alt=""/>
-      </a>
-
-      <div className="flex-grow-1 ms-2">
-        <div className="d-flex justify-content-between">
-          <div>                         
-            <p className="text-at-blue mb-0">Hammed Daudu <span className="text-muted">replied</span></p>
-            <small className="fst-italic acx-fs-8">16 days ago (Sat, 13 Mar 2021 at 12:45 PM)</small>
-          </div>
-
-          
-        </div>
-        
-      </div>
-
-    </div>
-    
-  </div>
-
-  <div className="px-md-3 pb-md-3">
-    
-
-    <div className="border rounded-2">
-      
-      {/* <!-- addressbar --> */}
-      <div className="address-wpr d-flex flex-wrap border-bottom py-1">
-        
-        <input type="hidden" value="foobar" className="address-value"/>
-        <div className="input-group ">
-          <div className="input-group-text border-0 bg-transparent p-0">
-            <button type="button" className="btn btn-sm border-0">To:</button>
-          </div>
-
-          <input type="text" name="" id="addressInput" className="address-input flex-grow-1 border-0"/>
-
-          <div className="input-group-text border-0 bg-transparent p-0">
-            <button data-address-field="addressccContainer" type="button" className="addressButton btn btn-sm border-0">cc</button>
-          </div>
-
-          <div className="input-group-text border-0 bg-transparent p-0">
-            <button data-address-field="addressbcContainer" type="button" className="addressButton btn btn-sm border-0">bb</button>
-          </div>
-        </div>
-
-        <div id="addressccContainer" className="input-group hider">
-          <div className="input-group-text border-0 bg-transparent p-0">
-            <button type="button" className="btn btn-sm border-0">cc:</button>
-          </div>
-
-          <input type="text" name="" className="address-input flex-grow-1 border-0"/>
-        </div>
-
-        <div id="addressbcContainer" className="input-group hider">
-          <div className="input-group-text border-0 bg-transparent p-0">
-            <button type="button" className="btn btn-sm border-0">bc:</button>
-          </div>
-
-          <input type="text" name="" className="address-input flex-grow-1 border-0"/>
-        </div>                    
-
-      </div>
-      {/* <!-- // addressbar --> */}
-
-      <div id="editor"></div>
-    </div>
-
-    {/* <!-- Send button --> */}
-    <div id="editorSendButton" className="btn-group dropup ms-auto position-relative">
-      <button type="button" className="btn btn-sm bg-at-blue-light px-2">Send</button>
-      <button type="button" className="btn btn-sm bg-at-blue-light px-2 border-start dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-        <span className="visually-hidden">Toggle Dropdown</span>
-      </button>
-      <ul className="dropdown-menu dropdown-menu-dark">
-        <li className="bg-at-blue-light px-2 rounded-1"><a className="dropdown-item text-white" href="#">Send and Close</a></li>
-        <li className="bg-at-blue-light px-2 rounded-1"><a className="dropdown-item text-white" href="#">Send as Comment</a></li>
-      </ul>
-    </div>
-
-  </div>                
-
-</div>
-</div>
 
                         
 
                     </div>
-                    {/* third column */}
-                    {/* <div>
-                      <UserProfile ticket={[currentTicket]}  />
-                    </div> */}
 
                 </div>}
 
