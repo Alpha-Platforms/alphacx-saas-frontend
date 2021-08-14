a72716acfa3abc3383cc3dd6b3da56c9ff9feb60
codeuiandy <frostandy41@gmail.com>
2021-08-14 19:38:11 +0100

    Added Auth to app

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -3,21 +3,51 @@ import Loader from "react-loader-spinner";
 import "react-responsive-modal/styles.css";
 import axios from "axios";
 import { getLocalItem } from "../components/helpers/authService";
+import jwtDecode from "jwt-decode";
 export const UserDataContext = createContext();
 
 export const UserDataProvider = (props) => {
   const [user, setUser] = useState();
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    RecallJwt();
+  }, []);
 
   useEffect(() => {
     getUserFromStorage();
   }, []);
+
+  // THIS CALLS JWT TOKEN EXP
+  // To custantly check if the user token is expired
+  const RecallJwt = () => {
+    try {
+      setInterval(async () => {
+        ValidateToken();
+      }, 5000);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const ValidateToken = () => {
+    let token = localStorage.getItem("token");
+    if (token == undefined || token == null || token == "") {
+      localStorage.clear();
+      return (window.location.href = "/");
+    }
+    if (jwtDecode(token).exp < Date.now() / 1000) {
+      localStorage.clear();
+      return (window.location.href = "/");
+    }
+    // console.log("still valid");
+  };
+
   const getUserFromStorage = () => {
     let lUser = getLocalItem("user");
-    //alert(lUser);
     if (lUser == undefined || lUser == null) {
-      // window.location.href = "/";
+      return;
     } else {
-      console.log(lUser);
       setUser(lUser);
     }
   };
