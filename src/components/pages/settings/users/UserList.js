5b2f522c9073e93bf70e6553ed782bcbcf00422d
Gideon Idoko <iamgideonidoko@gmail.com>
2021-09-04 22:07:15 +0100

    fix: user table pagination

--- src/components/pages/settings/users/UserList.js
+++ src/components/pages/settings/users/UserList.js
@@ -53,6 +53,34 @@ const UserList = ({
     }
   }, [isAgentsLoaded]);
 
+  function AlphacxMTPagination2(props) {
+    const {
+      ActionsComponent,
+      onChangePage,
+      onChangeRowsPerPage,
+      ...tablePaginationProps
+    } = props;
+  
+    return (
+      <TablePagination
+        {...tablePaginationProps}
+        // @ts-expect-error onChangePage was renamed to onPageChange
+        onPageChange={onChangePage}
+        onRowsPerPageChange={onChangeRowsPerPage}
+        ActionsComponent={(subprops) => {
+          const { onPageChange, ...actionsComponentProps } = subprops;
+          return (
+            // @ts-expect-error ActionsComponent is provided by material-table
+            <ActionsComponent
+              {...actionsComponentProps}
+              onChangePage={onPageChange}
+            />
+          );
+        }}
+      />
+    );
+  }
+
   const AlphacxMTPagination = (props) => {
     const {
       ActionsComponent,
@@ -300,7 +328,7 @@ const UserList = ({
                 }}
                 components={
                   {
-                    // Pagination: AlphacxMTPagination
+                    Pagination: AlphacxMTPagination2
                   }
                 }
               />
