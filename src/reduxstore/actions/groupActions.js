87198dc032d5dabec643acbabf75fb3e2115caa0
Gideon Idoko <iamgideonidoko@gmail.com>
2021-09-06 04:27:51 +0100

    feat: add team update

--- src/reduxstore/actions/groupActions.js
+++ src/reduxstore/actions/groupActions.js
@@ -1,25 +1,67 @@
 import axios from 'axios';
 import * as types from '../types';
-import { config } from '../../config/keys';
-import { returnErrors } from './errorActions';
+import {config} from '../../config/keys';
+import {returnErrors} from './errorActions';
 import {userTokenConfig} from '../../helper';
 
 export const getGroups = () => (dispatch, getState) => {
-	if (!navigator.onLine) {
-		return;
-	}
-	dispatch(setGroupsLoading());
-	axios.get(`${config.stagingBaseUrl}/groups`, userTokenConfig(getState))
-		.then(res => dispatch({
-			type: types.GET_GROUPS,
-			payload: (res.data && res.data.status === "Success") ? res.data.data : []
-		}))
-		.catch(err => dispatch(returnErrors(err.response?.data, err.response?.status)));
+    if (!navigator.onLine) {
+        return;
+    }
+    dispatch(setGroupsLoading());
+    axios
+        .get(`${config.stagingBaseUrl}/groups`, userTokenConfig(getState))
+        .then(res => dispatch({
+            type: types.GET_GROUPS,
+            payload: (res.data && res.data.status === "Success")
+                ? res.data.data
+                : []
+        }))
+        .catch(err => dispatch(returnErrors(err.response
+            ?.data, err.response
+            ?.status)));
 }
 
+export const addGroup = (newGroup, success, failed) => (dispatch, getState) => {
+
+    //Request body
+    const body = JSON.stringify(newGroup);
+
+    axios
+        .post(`${config.stagingBaseUrl}/groups`, body, userTokenConfig(getState))
+        .then(res => {
+            dispatch({type: types.ADD_GROUP, payload: res.data});
+			success && success();
+			
+        })
+        .catch(err => {
+			dispatch(returnErrors(err.response
+				?.data, err.response
+				?.status));
+			failed && failed();
+		});
+}
+
+// valid redux action
+export const updateGroup = (groupId, newGroup, success, failed) => (dispatch, getState) => {
+
+    //Request body
+    const body = JSON.stringify(newGroup);
+
+    axios
+        .patch(`${config.stagingBaseUrl}/groups/${groupId}`, body, userTokenConfig(getState))
+        .then(res => {
+            success && success();
+        })
+        .catch(err => {
+            dispatch(returnErrors(err.response
+                ?.data, err.response
+                ?.status))
+            failed && failed();
+        });
+
+}
 
 export const setGroupsLoading = () => {
-	return {
-		type: types.GROUPS_LOADING
-	}
+    return {type: types.GROUPS_LOADING}
 }
