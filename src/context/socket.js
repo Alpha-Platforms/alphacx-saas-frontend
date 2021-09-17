21c1dd6c523ff7b23f382f9ac647ce2e586206c6
System Administrator <root@Kemes-M1.local>
2021-09-17 17:41:35 +0100

    fixed - uncomment

--- src/context/socket.js
+++ src/context/socket.js
@@ -24,41 +24,41 @@ export const SocketDataProvider = (props) => {
     }
   };
 
-  // AppSocket.createConnection = async () => {
-  //   if (AppSocket.io?.connected) return; // if there has been a connection before, return
-  //   //open a connection
-  //   var connectionOptions = {
-  //     "force new connection": true,
-  //     reconnectionAttempts: "Infinity",
-  //     timeout: 10000,
-  //     transports: ["websocket"], 
-  //   };
-  //   AppSocket.io = socketIO(
-  //     `${baseUrl}?access_token=${localStorage.token}`,
-  //     connectionOptions
-  //   );
-  //   //listen for connection
-  //   AppSocket.io.on("connect", () => {
-  //     console.log("connected to server");
-  //   });
-  //   //console.log(">>>a","gets here");
+  AppSocket.createConnection = async () => {
+    if (AppSocket.io?.connected) return; // if there has been a connection before, return
+    //open a connection
+    var connectionOptions = {
+      "force new connection": true,
+      reconnectionAttempts: "Infinity",
+      timeout: 10000,
+      transports: ["websocket"], 
+    };
+    AppSocket.io = socketIO(
+      `${baseUrl}?access_token=${localStorage.token}`,
+      connectionOptions
+    );
+    //listen for connection
+    AppSocket.io.on("connect", () => {
+      console.log("connected to server");
+    });
+    //console.log(">>>a","gets here");
     
-  //   AppSocket.io.emit(`join`, ({userId:user?.id}));
-  //   AppSocket.io.emit(`ws_tickets`, (wsTicketFilter));
-  // };
+    AppSocket.io.emit(`join`, ({userId:user?.id}));
+    AppSocket.io.emit(`ws_tickets`, (wsTicketFilter));
+  };
 
-  // AppSocket.sendRequest = async (path, payload) => {
-  //   if (AppSocket.io && AppSocket.io.connected === true) {
-  //     AppSocket.io.emit(path, payload);
-  //   } else {
-  //     console.log("App disconnected from server");
-  //   }
-  // };
+  AppSocket.sendRequest = async (path, payload) => {
+    if (AppSocket.io && AppSocket.io.connected === true) {
+      AppSocket.io.emit(path, payload);
+    } else {
+      console.log("App disconnected from server");
+    }
+  };
 
   return (
     <SocketDataContext.Provider
       value={{
-        // AppSocket,
+        AppSocket,
         wsTickets,
         setWsTicketFilter,
         wsTicketFilter,
