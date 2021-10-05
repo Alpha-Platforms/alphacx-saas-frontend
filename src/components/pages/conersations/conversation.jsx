f30794fb4d91062889034dabad35db65ebbd92dd
Message Akunna <message.ao@gmail.com>
2021-10-05 07:32:16 +0100

    send ratings link (feedback from users) on ticket closed

--- src/components/pages/conersations/conversation.jsx
+++ src/components/pages/conersations/conversation.jsx
@@ -394,7 +394,7 @@ export default function Conversation() {
     if(RSTicketStage.label === "Closed"){
       let base_url = window.location.origin;
       let complete_url = `${base_url}/feedback/${localStorage.domain}/${ticket[0].id}/${ticket[0].customer.id}`;
-      let rich_text = `<p>Please click on the link to rate this conversation <a href='${complete_url}'>rate us here</a></p>`;
+      let rich_text = `<p>Your ticket has been marked as closed, Please click on the link to rate this conversation <a href='${complete_url}'>rate us here</a></p>`;
       let rich_text_encode = rich_text;
       let ReplyTicket = {
         richText : rich_text_encode,
