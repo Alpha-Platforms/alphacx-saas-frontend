84cf1e3bd770839fa8fde7c7591b23efbd7bca9b
Message Akunna <message.ao@gmail.com>
2021-10-20 13:08:32 +0100

    group fixes

--- src/components/pages/settings/users/UserList.js
+++ src/components/pages/settings/users/UserList.js
@@ -39,7 +39,7 @@ const UserList = ({
   admins,
   supervisors,
   isAgentsLoaded,
-  // groups,
+  groups,
   negateActiveState,
   isAdminsLoaded,
   isSupervisorLoaded,
@@ -64,9 +64,9 @@ const UserList = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
 }, [isUserAuthenticated]);
 
-  useEffect(() => {
-    console.log(admins);
-  },[]);
+  // useEffect(() => {
+  //   console.log(admins);
+  // },[]);
   // useEffect(() => {
   //   setUserLoading(!agents);
   //   if (isAgentsLoaded) {
@@ -254,15 +254,15 @@ const UserList = ({
                   },
                   {
                     title: "Team",
-                    field: "groups",
-                    render: rowData => (
-                      <div className={"table-tags"}>
-                        {rowData.groups.map( (item, index) => 
-                          ( index <= 1 && (<span className="badge rounded-pill acx-bg-gray-30 px-3 py-2 me-1 my-1">{item.name}</span>) )
-                        )}
+                    field: "group",
+                    // render: rowData => (
+                    //   <div className={"table-tags"}>
+                    //     {rowData.groups.map( (item, index) => 
+                    //       ( index <= 1 && (<span className="badge rounded-pill acx-bg-gray-30 px-3 py-2 me-1 my-1">{item.name}</span>) )
+                    //     )}
 
-                        {(rowData.groups.length > 2)? <span className="badge rounded-pill text-muted border px-2 py-1 my-1">{`+${rowData.groups.length-2}`}</span> : ""}
-                      </div>),
+                    //     {(rowData.groups.length > 2)? <span className="badge rounded-pill text-muted border px-2 py-1 my-1">{`+${rowData.groups.length-2}`}</span> : ""}
+                    //   </div>),
                   },
                   {
                     title: "Created",
@@ -314,7 +314,7 @@ const UserList = ({
                     role,
                     company,
                     email,
-                    groups,
+                    group,
                     created_at,
                     isActivated,
                     id,
@@ -325,10 +325,10 @@ const UserList = ({
                     emailAddress: email,
                     role,
                     // group: 'Head Office',
-                    // groups: groups.filter((x) => x.id === group_id)[0]?.name
-                    //   ? groups.filter((x) => x.id === group_id)[0]?.name
-                    //   : "",
-                    groups,
+                    group: groups.filter((x) => x.id === group_id)[0]?.name
+                      ? groups.filter((x) => x.id === group_id)[0]?.name
+                      : "",
+                    // groups,
                     created: moment(created_at).format('DD MMM, YYYY'),
                     // created: "13 Apr 2021",
                     contact: { firstname, lastname, id, avatar },
@@ -404,7 +404,7 @@ const mapStateToProps = (state, ownProps) => ({
   isAgentsLoaded: state.agent.isAgentsLoaded,
   isAdminsLoaded: state.admin.isAdminsLoaded,
   isSupervisorLoaded: state.supervisor.isSupervisorsLoaded,
-  // groups: state.group.groups,
+  groups: state.group.groups,
   isUserAuthenticated: state.userAuth.isUserAuthenticated
 });
 
