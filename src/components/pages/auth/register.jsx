593094121432f1d97d43d28e2c5360d2c6938b71
Keme Kenneth <keme.kenneth@gmail.com>
2021-12-02 03:39:54 +0100

    cast domain, email to lowercase

--- src/components/pages/auth/register.jsx
+++ src/components/pages/auth/register.jsx
@@ -67,11 +67,11 @@ const Login = ({history}) => {
         const validateEmail = ValidateEmail (userInput.email);
 
         const data = {
-            domain: userInput.domain,
+            domain: userInput.domain.toLowerCase(),
             firstname: userInput.firstName,
             lastname: userInput.lastName,
             companyName: userInput.companyName,
-            email: userInput.email,
+            email: userInput.email.toLowerCase(),
             password: userInput.password,
             country: userInput.country,
             currency: "Naira",
