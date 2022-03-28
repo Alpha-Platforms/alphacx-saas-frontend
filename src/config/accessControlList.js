728c04848c5fe3f866e9d53bacdd0aadc250f6a1
Keme Kenneth <keme.kenneth@gmail.com>
2022-03-28 17:46:13 +0100

    tenants route in acl

--- src/config/accessControlList.js
+++ src/config/accessControlList.js
@@ -1,6 +1,6 @@
 // @ts-nocheck
 const accessControlList = {
-    "Supervisor": ["Personal Information", "Settings Menu", "Team Settings", "Reports", "User Settings", "Account", "Automation Settings", "Role Settings", "Fields Settings", "Ticket Settings", "Livechat Settings", "SMS Integration", "Email Integration", "Notification Management", "Integration Settings", "Facebook Integration", "Whatsapp Integration", "Knowledge Base", "Ratings and Feedback", "Email Settings", "Twitter Integration", "Customer Search", "Customer Details"],
+    "Supervisor": ["Personal Information", "Settings Menu", "Team Settings", "Reports", "User Settings", "Account", "Automation Settings", "Role Settings", "Fields Settings", "Ticket Settings", "Livechat Settings", "SMS Integration", "Email Integration", "Notification Management", "Integration Settings", "Facebook Integration", "Whatsapp Integration", "Knowledge Base", "Ratings and Feedback", "Email Settings", "Twitter Integration", "Tenant Accounts", "Customer Search", "Customer Details"],
     get Administrator(){ return this.Supervisor},
     "Agent": ["Personal Information", "Settings Menu", "Knowledge Base", "Customer Search", "Customer Details"],
     "Observer": ["Personal Information", "Settings Menu", "Knowledge Base"]
