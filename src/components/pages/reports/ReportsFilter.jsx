f86b39dd5c9eb28317955e42e26196366e885a24
Gideon Idoko <iamgideonidoko@gmail.com>
2022-04-11 05:34:09 +0100

    Add ticket export to ticket filter

--- src/components/pages/reports/ReportsFilter.jsx
+++ src/components/pages/reports/ReportsFilter.jsx
@@ -17,6 +17,8 @@ import { TablePagination } from "@material-ui/core";
 import { httpGetMain } from '../../../helpers/httpMethods';
 import ScaleLoader from "react-spinners/ScaleLoader";
 import { NotificationManager } from 'react-notifications';
+import { ExportDropdown } from '../tickets/TicketList';
+import { exportTable, textCapitalize } from "../../../helper";
 
 const tableTheme = createTheme({
   palette: {
@@ -71,7 +73,7 @@ const getStatusColor = (status, id) => {
 
 
 // ticket tables
-const Tickets = ({ tickets, meta, handleFilterApply }) => {
+const Tickets = ({ tickets, meta, handleFilterApply, tableColumns, handleSelectionChange }) => {
 
   const AlphacxMTPagination = (props) => {
     const {
@@ -114,6 +116,111 @@ const Tickets = ({ tickets, meta, handleFilterApply }) => {
     );
   };
 
+  return (
+    <div
+          id="alphacxMTable"
+          className="pb-5 acx-ticket-cust-table acx-ticket-table fit-content"
+        >
+          {tickets && (
+            <MuiThemeProvider theme={tableTheme}>
+              <MaterialTable
+                title=""
+                icons={tableIcons}
+                columns={tableColumns}
+                data={tickets.map(
+                  ({
+                    customer,
+                    subject,
+                    id,
+                    category,
+                    created_at,
+                    status,
+                    assignee,
+                    rating,
+                    ticket_id
+                  }) => ({
+                    key: ticket_id,
+                    name: `${customer?.firstname} ${customer?.lastname === "default"? "" : customer?.lastname || ""}`,
+                    customerId: customer?.id,
+                    ticketId: ticket_id,
+                    ticketUid: id,
+                    email: customer?.email,
+                    subject: `${subject?.substr(0, 25)}...`,
+                    category: category?.name,
+                    created: dayjs(created_at).format("DD MMM, YYYY"),
+                    status: status?.status,
+                    assignedTo: `${assignee?.firstname || ""} ${assignee?.lastname || ""}`,
+                    rating: rating,
+                    assigneeId: assignee?.id,
+                    statusId: status?.id,
+                    createdTime: new Date(created_at || Date.now()).getTime()
+                  })
+                )}
+                options={{
+                  search: false,
+                  selection: true,
+                  // exportButton: true,
+                  tableLayout: "auto",
+                  paging: true,
+                  pageSize: tickets?.length === 0 ? 10 : (meta?.itemsPerPage || 50),
+                  headerStyle: {
+                    // backgroundColor: '#f8f9fa'
+                    backgroundColor: "#fefdfd",
+                  },
+                }}
+                components={{
+                  Pagination: AlphacxMTPagination,
+                }}
+                localization={{
+                  body: {
+                    emptyDataSourceMessage: "No tickets to display",
+                  },
+                }}
+                onSelectionChange={handleSelectionChange}
+              />
+            </MuiThemeProvider>
+          )}
+        </div>
+  );
+}
+
+// const get
+
+const ReportsFilter = () => {
+  const [dropdownActive, setDropdownActive] = useState(false);
+  const [filters, setFilters] = useState([]);
+  const [ticketData, setTicketData] = useState({ tickets: null, meta: null });
+  const [loading, setLoading] = useState(false);
+  let selectedRows = [];
+
+  console.log('selectedRows => ', selectedRows);
+
+  const handleSelectionChange = (rows) => {
+    // if (!multiIncludes(accessControlFunctions[user?.role], ["delete_ticket"])) return
+    
+    selectedRows = rows;
+
+    // const deleteBtnWrapper = window.document.querySelector('#delete-btn-wrapper');
+    // const selectedRowCount = window.document.querySelector('#selected-row-count');
+    // const ticketDeleteBtn = window.document.querySelector('#ticket-delete-btn');
+
+    // setTimeout(() => setRowsSelected(rows), 2000);
+    // selectedRowCount.innerHTML = rows.length;
+
+    // // show or hide button base on selected row count
+    // if (rows.length > 0) {
+    //   deleteBtnWrapper?.classList.remove('d-none');
+    // } else {
+    //   deleteBtnWrapper?.classList.add('d-none');
+    // }
+
+    // if (rows.length === 1) {
+    //   ticketDeleteBtn.disabled = false;
+    // } else {
+    //   ticketDeleteBtn.disabled = false;
+    // }
+  };
+
   const getRatingStar = (rating = 0) => {
     
     if (!rating) {
@@ -139,7 +246,7 @@ const Tickets = ({ tickets, meta, handleFilterApply }) => {
               {ratingArr.map((x, index) => <span key={index} className="table-ratings-span">{x ? <StarYellowSvg /> : <StarUnactiveSvg />}</span>)}
           </div>);
   }
-  
+
   const tableColumns = [
     {
       title: "Name",
@@ -206,81 +313,101 @@ const Tickets = ({ tickets, meta, handleFilterApply }) => {
     },
   ];
 
-  return (
-    <div
-          id="alphacxMTable"
-          className="pb-5 acx-ticket-cust-table acx-ticket-table fit-content"
-        >
-          {tickets && (
-            <MuiThemeProvider theme={tableTheme}>
-              <MaterialTable
-                title=""
-                icons={tableIcons}
-                columns={tableColumns}
-                data={tickets.map(
-                  ({
-                    customer,
-                    subject,
-                    id,
-                    category,
-                    created_at,
-                    status,
-                    assignee,
-                    rating,
-                    ticket_id
-                  }) => ({
-                    key: ticket_id,
-                    name: `${customer?.firstname} ${customer?.lastname === "default"? "" : customer?.lastname || ""}`,
-                    customerId: customer?.id,
-                    ticketId: ticket_id,
-                    ticketUid: id,
-                    email: customer?.email,
-                    subject: `${subject?.substr(0, 25)}...`,
-                    category: category?.name,
-                    created: dayjs(created_at).format("DD MMM, YYYY"),
-                    status: status?.status,
-                    assignedTo: `${assignee?.firstname || ""} ${assignee?.lastname || ""}`,
-                    rating: rating,
-                    assigneeId: assignee?.id,
-                    statusId: status?.id,
-                    createdTime: new Date(created_at || Date.now()).getTime()
-                  })
-                )}
-                options={{
-                  search: false,
-                  selection: true,
-                  // exportButton: true,
-                  tableLayout: "auto",
-                  paging: true,
-                  pageSize: tickets?.length === 0 ? 10 : (meta?.itemsPerPage || 50),
-                  headerStyle: {
-                    // backgroundColor: '#f8f9fa'
-                    backgroundColor: "#fefdfd",
-                  },
-                }}
-                components={{
-                  Pagination: AlphacxMTPagination,
-                }}
-                localization={{
-                  body: {
-                    emptyDataSourceMessage: "No tickets to display",
-                  },
-                }}
-                // onSelectionChange={handleSelectionChange}
-              />
-            </MuiThemeProvider>
-          )}
-        </div>
-  );
-}
+  const handleCSVExport = async (type) => {
+    if (ticketData.tickets) {
+      if (type === "selected") {
+        const data =
+        selectedRows.length !== 0
+          ? selectedRows
+          : ticketData.tickets.map(
+              ({ customer, subject, id, category, created_at, status, assignee, rating, ticket_id }) => ({
+                name: `${customer.firstname} ${customer.lastname == "default" ? "" : customer.lastname || "" }`,
+                email: customer.email,
+                subject: `${subject.substr(0, 25)}...`,
+                ticketUid: id,
+                ticketId: ticket_id,
+                category: category.name,
+                created: dayjs(created_at).format("DD MMM, YYYY"),
+                state: status,
+                assignedTo: textCapitalize(`${assignee?.firstname || ""} ${assignee?.lastname || ""}`),
+                rating: rating || 0
+              })
+            );
+        exportTable(tableColumns, data, "csv", "TicketExport");
+      } else if (type === "all") {
+        setLoading(true);
+        const res = await httpGetMain(`tickets?per_page=${ticketData.meta?.totalItems}&page=${1}`);
+        setLoading(false);
+        if (res?.status === 'success') {
+          const data = res?.data?.tickets?.map(
+            ({ customer, subject, id, category, created_at, status, assignee, rating, ticket_id  }) => ({
+              name: `${textCapitalize(customer?.firstname || '')} ${textCapitalize(customer.lastname === "default"? "" : customer.lastname ? customer?.lastname : '')}`,
+              email: customer.email,
+              subject: `${subject.substr(0, 25)}...`,
+              // ticketId: id.slice(-8),
+              ticketId: ticket_id,
+              category: category.name,
+              created: dayjs(created_at).format("DD MMM, YYYY"),
+              state: status,
+              assignedTo: textCapitalize(`${assignee?.firstname || ""} ${assignee?.lastname || ""}`),
+              rating: rating || 0
+            })
+          );
+          exportTable(tableColumns, data, "csv", "TicketExport");
+        } else {
+          NotificationManager.error('Tickets could not be retrieved', 'Error');
+        }
+      }
+    }
+  }
 
-// const get
-
-const ReportsFilter = () => {
-  const [dropdownActive, setDropdownActive] = useState(false);
-  const [filters, setFilters] = useState([]);
-  const [ticketData, setTicketData] = useState({ tickets: null, meta: null });
-  const [loading, setLoading] = useState(false);
+  const handlePDFExport = async (type) => {
+    if (ticketData.tickets) {
+      if (type === "selected") {
+        const data =
+          selectedRows.length !== 0
+            ? selectedRows
+            : ticketData.tickets.map(
+                ({ customer, subject, id, category, created_at, status, assignee, rating, ticket_id  }) => ({
+                  name: textCapitalize(`${customer.firstname} ${customer.lastname == "default"? "" : customer.lastname || ""}`),
+                  email: customer.email,
+                  subject: `${subject.substr(0, 25)}...`,
+                  // ticketId: id.slice(-8),
+                  ticketId: ticket_id,
+                  category: category.name,
+                  created: dayjs(created_at).format("DD MMM, YYYY"),
+                  state: status,
+                  assignedTo: textCapitalize(`${assignee?.firstname || ""} ${assignee?.lastname || ""}`),
+                  rating: rating || 0
+                })
+              );
+        exportTable(tableColumns, data, "pdf", "TicketExport");
+      } else if (type === "all") {
+        setLoading(true);
+        const res = await httpGetMain(`tickets?per_page=${ticketData.meta?.totalItems}&page=${1}`);
+        setLoading(false);
+        if (res?.status === 'success') {
+          const data = res?.data?.tickets?.map(
+            ({ customer, subject, id, category, created_at, status, assignee, rating, ticket_id  }) => ({
+              name: `${textCapitalize(customer?.firstname || 'Firstname')} ${textCapitalize(customer.lastname === "default"? "" : customer.lastname ? customer?.lastname : '')}`,
+              email: customer.email,
+              subject: `${subject.substr(0, 25)}...`,
+              // ticketId: id.slice(-8),
+              ticketId: ticket_id,
+              category: category.name,
+              created: dayjs(created_at).format("DD MMM, YYYY"),
+              state: status,
+              assignedTo: textCapitalize(`${assignee?.firstname || ""} ${assignee?.lastname || ""}`),
+              rating: rating || 0
+            })
+          );
+          exportTable(tableColumns, data, "pdf", "TicketExport");
+        } else {
+          NotificationManager.error('Tickets could not be retrieved', 'Error');
+        }
+      }
+    }
+  };
   
   useEffect(() => {
     const filterDropdown = window.document.querySelector('.filter-dropdown');
@@ -321,7 +448,29 @@ const ReportsFilter = () => {
       <FilterDropdown active={dropdownActive} setFilters={setFilters} />
 
       <div className={`reports-filter-body ${ticketData.tickets ? 'active': ''}`}>
-        {ticketData?.tickets && <Tickets handleFilterApply={handleFilterApply} tickets={ticketData.tickets} meta={ticketData.meta} />}
+        {ticketData?.tickets && (<>
+          <div
+            style={{ background: "#fefdfd" }}
+            className={``}
+          >
+
+            <div className="btn-toolbar mb-md-0">
+              <ExportDropdown handlePDFExport={handlePDFExport} handleCSVExport={handleCSVExport} />
+            </div>
+
+                {/* <div id="delete-btn-wrapper" className="delete-btn-wrapper d-none">
+                  <div>
+                    <button onClick={openTicketDeleteModal} id="ticket-delete-btn" className="btn" type="button"><i className="bi bi-trash"></i></button>
+                  </div>
+                  <div>
+                    <span><span id="selected-row-count">0</span> ticket(s) selected</span>
+                  </div>
+              </div> */}
+          </div>
+          <div className="ticket-table-wrapper">
+            <Tickets handleFilterApply={handleFilterApply} tickets={ticketData.tickets} meta={ticketData.meta} tableColumns={tableColumns} handleSelectionChange={handleSelectionChange} />
+          </div>
+        </>)}
       </div>
       {(loading) && (
         <div
