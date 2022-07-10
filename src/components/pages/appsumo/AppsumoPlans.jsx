4b226309c0ffdb828378e6309ccfeb7d3a234042
Gideon Idoko <iamgideonidoko@gmail.com>
2022-07-10 07:36:34 +0100

    Style appsumo plan page

--- src/components/pages/appsumo/AppsumoPlans.jsx
+++ src/components/pages/appsumo/AppsumoPlans.jsx
@@ -29,21 +29,23 @@ function AppsumoPlans() {
         'Dedicated Account Manager',
     ];
     return (
-        <div>
-            <div className="section--one">
+        <div className="appsumo-plans-page">
+            <div className="appsumo__section--one">
                 <h1>All-in-one Subscription Plan</h1>
                 <p>
                     Get started with our all-in-one standard Alpha subscription; and an enterprise plan for
                     organisations with specific needs.
                 </p>
             </div>
-            <div className="section--two">
-                <div className="appsumo__card--free">
+            <div className="appsumo__section--two">
+                <div className="appsumo__card--free appsumo__card">
                     <h4>Free</h4>
                     <h2>NGN 0</h2>
                     <p>Forever free</p>
-                    <button type="button">Start Free Trial</button>
-                    <ul>
+                    <button className="appsumo__card--cta btn" type="button">
+                        Start Free Trial
+                    </button>
+                    <ul className="appsumo__card--features">
                         {freePlanFeatures.map((item, idx) => (
                             <li key={idx}>
                                 <span>
@@ -54,38 +56,42 @@ function AppsumoPlans() {
                         ))}
                     </ul>
                 </div>
-            </div>
-            <div className="appsumo__card--alpha">
-                <h4>Alpha Plan</h4>
-                <h2>$15</h2>
-                <p>Per Agent/Month - Billed Annually</p>
-                <button type="button">Start Free Trial</button>
-                <ul>
-                    {alphaPlanFeatures.map((item, idx) => (
-                        <li key={idx}>
-                            <span>
-                                <TickeCircle color="#C20C38" />
-                            </span>
-                            <span>{item}</span>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            <div className="appsumo__card--enterprise">
-                <h4>Enterprise Plan</h4>
-                <h2>Customisation</h2>
-                <p>Build the right plan </p>
-                <button type="button">Contact Us</button>
-                <ul>
-                    {enterprisePlanFeatures.map((item, idx) => (
-                        <li key={idx}>
-                            <span>
-                                <TickeCircle color="#fffff" />
-                            </span>
-                            <span>{item}</span>
-                        </li>
-                    ))}
-                </ul>
+                <div className="appsumo__card--alpha appsumo__card">
+                    <h4>Alpha Plan</h4>
+                    <h2>$15</h2>
+                    <p>Per Agent/Month - Billed Annually</p>
+                    <button className="appsumo__card--cta btn" type="button">
+                        Start Free Trial
+                    </button>
+                    <ul className="appsumo__card--features">
+                        {alphaPlanFeatures.map((item, idx) => (
+                            <li key={idx}>
+                                <span>
+                                    <TickeCircle color="#C20C38" />
+                                </span>
+                                <span>{item}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="appsumo__card--enterprise appsumo__card">
+                    <h4>Enterprise Plan</h4>
+                    <h2>Customisation</h2>
+                    <p>Build the right plan </p>
+                    <button className="appsumo__card--cta btn" type="button">
+                        Contact Us
+                    </button>
+                    <ul className="appsumo__card--features">
+                        {enterprisePlanFeatures.map((item, idx) => (
+                            <li key={idx}>
+                                <span>
+                                    <TickeCircle color="#ffffff" />
+                                </span>
+                                <span>{item}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </div>
     );
