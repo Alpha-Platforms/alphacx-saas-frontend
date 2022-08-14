0dd528954416e2d0a53bbc0903c8bad25aeb5449
Keme Kenneth <keme.kenneth@gmail.com>
2022-08-14 20:59:43 +0100

    placeholders branching

---
+++ src/components/pages/settings/notifications/components/placeholders.js
@@ -0,0 +1,86 @@
+const allPlaceholders = {
+    agentEmailAutoRespond: [
+        {
+            title: 'Ticket ID',
+            placeHolder: 'ticketId',
+        },
+        {
+            title: 'SLA Duration',
+            placeHolder: 'slaDuration',
+        },
+    ],
+    statusAutoResponse: [
+        {
+            title: 'Ticket ID',
+            placeHolder: 'ticketId',
+        },
+        {
+            title: 'Customer',
+            placeHolder: 'customerName',
+        },
+        {
+            title: 'Status',
+            placeHolder: 'status',
+        },
+        {
+            title: 'Agent',
+            placeHolder: 'agentName',
+        },
+        {
+            title: 'Subject',
+            placeHolder: 'subject',
+        },
+    ],
+    mentionEmailNotification: [
+        {
+            title: 'Ticket ID',
+            placeHolder: 'ticketId',
+        },
+        {
+            title: 'Name',
+            placeHolder: 'name',
+        },
+    ],
+    customerInitiateResponse: [
+        {
+            title: 'Ticket ID',
+            placeHolder: 'ticketId',
+        },
+        {
+            title: 'Customer',
+            placeHolder: 'customerName',
+        },
+        {
+            title: 'Status',
+            placeHolder: 'status',
+        },
+        {
+            title: 'Subject',
+            placeHolder: 'subject',
+        }
+    ],
+    ticketStatusClosed: [
+        {
+            title: 'Ticket ID',
+            placeHolder: 'ticketId',
+        },
+        {
+            title: 'Customer',
+            placeHolder: 'customerName',
+        },
+        {
+            title: 'Status',
+            placeHolder: 'status',
+        },
+        {
+            title: 'Agent',
+            placeHolder: 'agentName',
+        },
+        {
+            title: 'Subject',
+            placeHolder: 'subject',
+        },
+    ],
+};
+
+export default allPlaceholders;
