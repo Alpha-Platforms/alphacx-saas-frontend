eb055284c6e84257d34b90494d4c48bf32f90ebb
Gideon Idoko <iamgideonidoko@gmail.com>
2022-08-17 04:48:36 +0100

    Add utility function to lighten or darken hex color

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-bitwise */
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/jsx-filename-extension */
 // @ts-nocheck
@@ -416,4 +417,36 @@ export const createEvent = (eventName, data, options = {}) => {
     const eventOptions = options && isValidObject(options) ? options : {};
     const event = new CustomEvent(eventName, { detail: data, ...eventOptions });
     window.document.dispatchEvent(event);
+};
+
+/**
+ * Lighten (+ve) or darken (-ve) color.
+ */
+export const lightenColor = (col = '#000000', amt = 0) => {
+    let usePound = false;
+
+    if (col[0] === '#') {
+        // eslint-disable-next-line no-param-reassign
+        col = col.slice(1);
+        usePound = true;
+    }
+
+    const num = parseInt(col, 16);
+
+    let r = (num >> 16) + amt;
+
+    if (r > 255) r = 255;
+    else if (r < 0) r = 0;
+
+    let b = ((num >> 8) & 0x00ff) + amt;
+
+    if (b > 255) b = 255;
+    else if (b < 0) b = 0;
+
+    let g = (num & 0x0000ff) + amt;
+
+    if (g > 255) g = 255;
+    else if (g < 0) g = 0;
+
+    return (usePound ? '#' : '') + (g | (b << 8) | (r << 16)).toString(16);
 };
