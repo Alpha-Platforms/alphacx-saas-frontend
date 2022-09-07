c4d31d17d14d6a4717dc5e82367199ba7336a700
Gideon Idoko <iamgideonidoko@gmail.com>
2022-09-07 06:40:49 +0100

    Add wa connection button

--- src/components/pages/settings/social_integrations/whatsapp/WhatsappCx.jsx
+++ src/components/pages/settings/social_integrations/whatsapp/WhatsappCx.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { css } from '@emotion/css';
+import Spinner from 'react-bootstrap/Spinner';
 
 function WhatsappCx() {
+    const [isInitiating, setIsInitiating] = useState(false);
+
+    const initiateWhatsapp = () => {
+        console.log('Attempting to initiate whatsapp');
+    };
     return (
         <>
             <p className="mt-3">
                 <strong>Connect via WhatsappCX</strong>
             </p>
 
-            <div className="mt-4 mb-5 col-md-8" />
+            <div className="mt-5 mb-5 col-md-8">
+                <button
+                    type="button"
+                    className={`btn ${css({ backgroundColor: '#25D366', color: 'white' })}`}
+                    onClick={initiateWhatsapp}
+                >
+                    {isInitiating ? (
+                        <span className="text-light d-flex justify-content-center align-items-center">
+                            <Spinner
+                                as="span"
+                                size="sm"
+                                animation="border"
+                                variant="light"
+                                aria-hidden="true"
+                                role="status"
+                            />
+                            <span className="ms-1"> Loading...</span>
+                        </span>
+                    ) : (
+                        <span className="">
+                            <i className="bi-whatsapp" /> Connect account
+                        </span>
+                    )}
+                </button>
+            </div>
         </>
     );
 }
