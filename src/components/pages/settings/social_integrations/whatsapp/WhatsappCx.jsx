5462e23b700a8f211658632b942b8da6e8c8c9fd
Gideon Idoko <iamgideonidoko@gmail.com>
2022-09-07 05:53:52 +0100

    Fix typo in app socket class file

--- src/components/pages/settings/social_integrations/whatsapp/WhatsappCx.jsx
+++ src/components/pages/settings/social_integrations/whatsapp/WhatsappCx.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function WhatsappCx() {
-    return <div>WhatsappCx</div>;
+    return (
+        <>
+            <p className="mt-3">
+                <strong>Connect via WhatsappCX</strong>
+            </p>
+
+            <div className="mt-4 mb-5 col-md-8" />
+        </>
+    );
 }
 
 export default WhatsappCx;
